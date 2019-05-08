afe396465dc7b834afb6f2c733c27e868a724e16
Margarita Obraztsova <marharyta.obraztsova@gmail.com>
2019-05-08 22:43:01 +0300

    Fix import statement typo

--- serverEvent.js
+++ serverEvent.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 const scrapeAsosProductPage = require("./asosProductPageScraper");
-const dbManager = require("./mongoDBManager");
+const dbManager = require("./loginMongoDBManager");
 const asosDBManager = require("./asosMongoDBManager");
 const nodemailer = require("nodemailer");
 
