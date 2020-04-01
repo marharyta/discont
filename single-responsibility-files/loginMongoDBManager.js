34c6523d0472a32b3e98a50c4506cb89d21970bc
Margarita Obraztsova <margaritaobraztsova@Margaritas-MacBook-Pro.local>
2020-04-01 15:59:31 +0300

    Modify login logic

--- single-responsibility-files/loginMongoDBManager.js
+++ single-responsibility-files/loginMongoDBManager.js
@@ -13,29 +13,28 @@ const user = new Schema({
 const AppUsers = mongoose.model("users", user);
 
 function loginPerson(login, password) {
-  return mongoose.connect(process.env.moongoDBLink).then(d => {
+  mongoose.connect(process.env.moongoDBLink).then(d => {
     console.log("connection opened");
+  });
+  return new Promise(function(resolve, reject) {
+    AppUsers.findOne({ login: login, password: password })
+      .then(data => {
+        console.log("we logged in!", data);
 
-    return new Promise(function(resolve, reject) {
-      AppUsers.findOne({ login: login, password: password })
-        .then(data => {
-          console.log("we logged in!", data);
-
-          mongoose.disconnect().then(d => {
-            console.log("conection closed ");
-          });
-
-          if (data.login === login && password === password) {
-            resolve(data);
-          } else {
-            reject();
-          }
-        })
-        .catch(e => {
-          console.log("error trying to log in ", e);
-          reject();
+        mongoose.disconnect().then(d => {
+          console.log("conection closed ");
         });
-    });
+
+        if (data.login === login && password === password) {
+          resolve(data);
+        } else {
+          reject();
+        }
+      })
+      .catch(e => {
+        console.log("error trying to log in ", e);
+        reject();
+      });
   });
 }
 
